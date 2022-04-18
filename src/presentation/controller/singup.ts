eef03f6c8772e8bad26bcb16f1ce771a29890c77
leonardo q <leonardoq.leao@outlook.com>
2022-04-18 09:59:03 -0300

    feat: ensure SingUpController return 400 if no passawordConfirmation providaded

--- src/presentation/controller/singup.ts
+++ src/presentation/controller/singup.ts
@@ -5,7 +5,7 @@ import { badResponse } from '../helpers/http-helpers'
 export class SignUpController {
   handle (httpRequest: httpRequest): httpResponse {
     // Verificação dos campos faltando, caso algum campo falte, o singup.spec vai acusar.
-    const requireField = ['name', 'email', 'password']
+    const requireField = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requireField) {
       if (!httpRequest.body[field]) {
         return badResponse(new MissingParamError(field))
