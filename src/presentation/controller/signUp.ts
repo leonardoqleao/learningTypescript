9ce2b23ee9e3416161b11e58d6bf61036a00187a
leonardo q <leonardoq.leao@outlook.com>
2022-04-18 22:30:46 -0300

    feat: ensure SignUpControler return 500 if EmailValidator throws

--- src/presentation/controller/signUp.ts
+++ src/presentation/controller/signUp.ts
@@ -1,9 +1,10 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
-import { MissingParamError } from '../erros/missingParamErro'
-import { InvalidParamError } from '../erros/invalidParamErro'
-import { badRquest } from '../helpers/http-helpers'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/invalidEmail'
+import { MissingParamError } from '../erros/missingParamErro'
+import { InvalidParamError } from '../erros/invalidParamErro'
+import { ServerError } from '../erros/serverErro'
+import { badRquest } from '../helpers/http-helpers'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -13,16 +14,23 @@ export class SignUpController implements Controller {
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    // Verificação dos campos faltando, caso algum campo falte, o singup.spec vai acusar.
-    const requireField = ['name', 'email', 'password', 'passwordConfirmation']
-    for (const field of requireField) {
-      if (!httpRequest.body[field]) {
-        return badRquest(new MissingParamError(field))
+    try {
+      // Verificação dos campos faltando, caso algum campo falte, o singup.spec vai acusar.
+      const requireField = ['name', 'email', 'password', 'passwordConfirmation']
+      for (const field of requireField) {
+        if (!httpRequest.body[field]) {
+          return badRquest(new MissingParamError(field))
+        }
       }
-    }
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
-    if (!isValid) {
-      return badRquest(new InvalidParamError('email'))
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!isValid) {
+        return badRquest(new InvalidParamError('email'))
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
   }
 }
