83d8921b061143a96f896a7e82ce26cba02d880a
leonardo q <leonardoq.leao@outlook.com>
2022-04-18 21:33:29 -0300

    feat: ensuer SignUpController returns 400 if an invalid email is providad

--- src/presentation/controller/signUp.ts
+++ src/presentation/controller/signUp.ts
@@ -1,16 +1,28 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../erros/missingParamErro'
-import { badResponse } from '../helpers/http-helpers'
+import { InvalidParamError } from '../erros/invalidParamErro'
+import { badRquest } from '../helpers/http-helpers'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/invalidEmail'
 
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     // Verificação dos campos faltando, caso algum campo falte, o singup.spec vai acusar.
     const requireField = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requireField) {
       if (!httpRequest.body[field]) {
-        return badResponse(new MissingParamError(field))
+        return badRquest(new MissingParamError(field))
       }
+    }
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRquest(new InvalidParamError('email'))
     }
   }
 }
